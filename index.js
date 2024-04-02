0d173520968e541edfaf710b5ac2a4c38a88998a
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-02 13:09:46 -0400

    first iteration
    WQL
    q
    wq
    wq

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    let x = number - 42
+    return Math.abs(x)
+}
+function distanceFromHqInFeet(number) {
+    let x = distanceFromHqInBlocks(number) * 264;
+    return x
+}
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end) * 264)
+}
+function calculatesFarePrice(start, end){
+    let distance = distanceTravelledInFeet(start, end)
+    let price
+    if (distance < 400) {
+        price = 0
+    }
+    if (distance > 400 && distance < 2000) {
+        price = (distance - 400) * 0.02
+    }
+    if (distance > 2000 && distance < 2500) {
+        price = 25
+    }
+    if (distance > 2500) {
+        price = 'cannot travel that far'
+    }
+    return price
+}
